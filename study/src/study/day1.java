43cd93ea73e3c7e151e6b56ee23bfbff16e0f8a9
juhyang lee <juhyang0310@gmail.com>
2021-09-12 10:58:12 +0900

    과제1

---
+++ study/src/study/day1.java
@@ -0,0 +1,15 @@
+package study;
+
+public class day1 {
+
+	public int solution(String s) {
+        int answer = 0;
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		 
+		
+	}
+
+}
