9abf4d89602ddca0661390545a4e04575920f4d4
juhyang lee <juhyang0310@gmail.com>
2021-09-25 03:42:31 +0900

    Day1

--- study/src/study/day1.java
+++ study/src/study/day1.java
@@ -9,11 +9,11 @@ public class day1 {
             return 1;
         }
 
-        for(int i = 1; i<=s.length()/2; i++){
+        for(int i = 1; i <= s.length()/2; i++){
             //압축카운팅, 기준 문자열, 비교 문자열
             int cnt = 1;
             String str1 = s.substring(0,i);
-            String str2;
+            String str2="";
             //최종 출력 문자열
             String result = "";
 
@@ -30,18 +30,16 @@ public class day1 {
                     if(cnt > 1){
                         result = result + cnt + str1;
                     } else {
-                        if(str1.length() == str2.length()){
-                            result = result + str1;
-                        } else {
-                            result = result + str1 + str2;
-                        }
+                        result = result + str1;
                     }
-                    //반복이 종료되었으므로 새로운 기준 문자열을 설정한다
-                    str1 = str2;
+                    //문자열의 반복이 종료되었으므로 새로운 기준 문자열을 설정한다
+                    if(str1.length() == str2.length()){str1 = str2;}
                     cnt = 1;
                 }
             }
-            System.out.println(result);
+            //나누어 떨어지지 않는 문자 처리
+            if(str1.length() != str2.length()){result = result + str2;}
+
             answer = Math.min(answer, result.length());
         }
         return answer;
@@ -50,7 +48,7 @@ public class day1 {
 	
 	public static void main(String[] args) {
         day1 a = new day1();
-		System.out.println(a.solution("aabbaccc"));
+		System.out.println(a.solution("xxxxxxxxxxyyy"));
 	}
 
 }
