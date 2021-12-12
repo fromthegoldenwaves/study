a8f35ab5133c56ab768267c35f0a2a72d45d1f0e
myounghee Kim <febhee00@gmail.com>
2021-12-12 18:11:36 +0900

    20211212

--- study/src/study/day9_K번쨰.java
+++ study/src/study/day9_K번쨰.java
@@ -6,7 +6,7 @@ class Solution {
         for(int i = 0 ; i < commands.length ; i++) {
             int[] list = new int[commands[i][1] - commands[i][0] + 1];
             int index = 0;
-            for(int j = commands[i][0] - 1 ; i < commands[i][1]; j++){
+            for(int j = commands[i][0] - 1 ; j < commands[i][1]; j++){
                 list[index] = array[j];
                 index++;
             }
