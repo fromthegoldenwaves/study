4af9e35bb4be42e3e9b4d837f0b9d899a82f191e
fromthegoldenwaves <juhyang0310@gmail.com>
2022-04-10 21:01:19 +0900

    가운데 글자 가져오기

---
+++ study/src/study/day25.java
@@ -0,0 +1,14 @@
+package study;
+
+public class day25 {
+    public String solution(String s) {
+        String answer = "";
+        int n = s.length()/2;
+        if (s.length()%2 == 0 ){
+            answer = s.substring(n, n+2);
+        } else {
+            answer = s.substring(n, n+1);
+        }
+        return answer;
+    }    
+}
